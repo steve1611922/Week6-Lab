dea59874e612e99c617e3e56e68d17fe2542984e
steve1611922 <1611922@rgu.ac.uk>
2017-03-07 16:39:09 +0000

    initial

--- displayXmenMovies.php
+++ displayXmenMovies.php
@@ -1,7 +1,27 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1611922
- * Date: 07/03/2017
- * Time: 15:59
- */
+<! DOCTYPE html >
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Marvel Movies after 2010</title>
+</head>
+<body>
+<p>
+    <?php
+    include('dbConnect.php'); // connect to db
+    // php code goes here
+    // create a SQL query as a string
+    $sql_query = "SELECT * FROM marvelmovies WHERE title like '%X%'";
+    // execute the SQL query
+    $result = $link->query($sql_query);
+    // iterate over $result object one $row at a time
+    // use fetch_array() to return an associative array
+    while($row = $result->fetch_array()){
+        // print out fields from row of data
+        $movieTitle = $row["title"];
+        echo "<p>" . $movieTitle . "</p>";
+    }
+    include('dbClose.php'); // disconnect from db
+    ?>
+</p>
+</body>
+</html>
