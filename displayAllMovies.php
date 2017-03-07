7ef30726362fcd50ef818971293eb29cad1ad49d
steve1611922 <1611922@rgu.ac.uk>
2017-03-07 16:24:43 +0000

    initial

--- displayAllMovies.php
+++ displayAllMovies.php
@@ -17,7 +17,8 @@
     // use fetch_array() to return an associative array
     while($row = $result->fetch_array()){
         // print out fields from row of data
-        echo "<p>” . $row('title') . “</p>";
+        $movieTitle = $row["title"];
+        echo "<p>" . $movieTitle . "</p>";
     }
     include('dbClose.php'); // disconnect from db
     ?>
