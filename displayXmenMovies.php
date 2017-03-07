3dc73b8ede5f7ea6651278bb53f017d3a1280413
steve1611922 <1611922@rgu.ac.uk>
2017-03-07 16:41:33 +0000

    initial

--- displayXmenMovies.php
+++ displayXmenMovies.php
@@ -10,7 +10,7 @@
     include('dbConnect.php'); // connect to db
     // php code goes here
     // create a SQL query as a string
-    $sql_query = "SELECT * FROM marvelmovies WHERE title like '%X%'";
+    $sql_query = "SELECT * FROM marvelmovies WHERE title like 'X'";
     // execute the SQL query
     $result = $link->query($sql_query);
     // iterate over $result object one $row at a time
