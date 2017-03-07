c2c30a88f81a15a6346b3ade5bbf79223641eecc
steve1611922 <1611922@rgu.ac.uk>
2017-03-07 16:21:24 +0000

    initial

--- displayAllMovies.php
+++ displayAllMovies.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Display all Movies</title>
 </head>
 <body>
 <p>
@@ -12,7 +12,7 @@
     // create a SQL query as a string
     $sql_query = "SELECT * FROM marvelmovies";
     // execute the SQL query
-    $result = $db->query($sql_query);
+    $result = $link->query($sql_query);
     // iterate over $result object one $row at a time
     // use fetch_array() to return an associative array
     while($row = $result->fetch_array()){
