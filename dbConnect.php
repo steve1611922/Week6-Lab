75518094772861ebe9bb39a7d5625b0b51e3ae2e
steve1611922 <1611922@rgu.ac.uk>
2017-03-07 16:30:09 +0000

    initial

--- dbConnect.php
+++ dbConnect.php
@@ -19,6 +19,6 @@ if (!$link) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
-echo "Connected to ".$connectstr_dbname;
+//echo "Connected to ".$connectstr_dbname;
 
 ?>
