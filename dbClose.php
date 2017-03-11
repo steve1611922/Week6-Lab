90d448f88c99a93d5e98003d87c835a8ddebf2a9
steve1611922 <1611922@rgu.ac.uk>
2017-03-11 13:59:10 +0000

    initial

--- dbClose.php
+++ dbClose.php
@@ -1,7 +1,8 @@
 <?php
     // mysqli_* close connection
-    $result->close();   // close any query
-    $link->close();     //close connection
+//    $result->close();   // close any query
+//    $link->close();     //close connection
+
 /*    // PDO close connection
     unset($result);
     unset($link_pdo);*/
