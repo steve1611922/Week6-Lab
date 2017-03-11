58927bb5ee7ed04cd9d7cb5815890d55b69b7744
steve1611922 <1611922@rgu.ac.uk>
2017-03-11 13:46:39 +0000

    initial

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <title>Week 6</title>
 </head>
 <body>
 <p>Week 6 Tutorials</p><br>
-<a href="displayAllMovies.php">Display All Movies</a><br>
+<a href="displayAllMovies.php" class="btn btn-info" role="button">Display All Movies</a><br>
 <a href="displayMarvelMovies.php">Display Movies created by Marvel Studios</a><br>
 <a href="displayMoviesAfter2010.php">IDisplay All Movies created after 2010</a><br>
 <a href="displayXmenMovies.php">Display all X-Men Films</a><br>
