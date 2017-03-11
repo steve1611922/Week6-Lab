20681f28639fe2a5679ab2d250abd347816fc976
steve1611922 <1611922@rgu.ac.uk>
2017-03-11 13:49:38 +0000

    initial

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
 <body>
 <p>Week 6 Tutorials</p><br>
 <a href="displayAllMovies.php" class="btn btn-info" role="button">Display All Movies</a><br>
-<a href="displayMarvelMovies.php">Display Movies created by Marvel Studios</a><br>
-<a href="displayMoviesAfter2010.php">IDisplay All Movies created after 2010</a><br>
-<a href="displayXmenMovies.php">Display all X-Men Films</a><br>
+<a href="displayMarvelMovies.php" class="btn btn-info" role="button">Display Movies created by Marvel Studios</a><br>
+<a href="displayMoviesAfter2010.php" class="btn btn-info" role="button">Display All Movies created after 2010</a><br>
+<a href="displayXmenMovies.php" class="btn btn-info" role="button">Display all X-Men Films</a><br>
 <br>
-<a href="simpleLoginForm.php">Simple login Form</a><br>
+<a href="simpleLoginForm.php" class="btn btn-info" role="button">Simple login Form</a><br>
 <br>
-<a href="imageUploadForm.php">Image Upload</a><br>
-<a href="view.php">View Image</a><br>
+<a href="imageUploadForm.php" class="btn btn-info" role="button">Image Upload</a><br>
+<a href="view.php" class="btn btn-info" role="button">View Image</a><br>
 
 <p>
     <?php
