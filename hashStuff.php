9a291811f960ccbd07e22e93b659f59a9b1e0321
steve1611922 <1611922@rgu.ac.uk>
2017-03-15 11:52:51 +0000

    initial

--- hashStuff.php
+++ hashStuff.php
@@ -6,3 +6,53 @@ foreach (hash_algos() as $v) {
     printf("%-12s %3d %s\n", $v, strlen($r), $r);
 }
 ?>
+<?php
+/**
+ * We just want to hash our password using the current DEFAULT algorithm.
+ * This is presently BCRYPT, and will produce a 60 character result.
+ *
+ * Beware that DEFAULT may change over time, so you would want to prepare
+ * By allowing your storage to expand past 60 characters (255 would be good)
+ */
+echo password_hash("hello", PASSWORD_DEFAULT)."\n";
+
+/**
+ * In this case, we want to increase the default cost for BCRYPT to 12.
+ * Note that we also switched to BCRYPT, which will always be 60 characters.
+ */
+$options = [
+    'cost' => 12,
+];
+echo password_hash("hello", PASSWORD_BCRYPT, $options)."\n";
+
+/**
+ * Note that the salt here is randomly generated.
+ * Never use a static salt or one that is not randomly generated.
+ *
+ * For the VAST majority of use-cases, let password_hash generate the salt randomly for you
+ */
+$options = [
+    'cost' => 11,
+    'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+];
+echo password_hash("hello", PASSWORD_BCRYPT, $options)."\n";
+
+/**
+ * This code will benchmark your server to determine how high of a cost you can
+ * afford. You want to set the highest cost that you can without slowing down
+ * you server too much. 8-10 is a good baseline, and more is good if your servers
+ * are fast enough. The code below aims for ≤ 50 milliseconds stretching time,
+ * which is a good baseline for systems handling interactive logins.
+ */
+$timeTarget = 0.05; // 50 milliseconds
+
+$cost = 8;
+do {
+    $cost++;
+    $start = microtime(true);
+    password_hash("test", PASSWORD_BCRYPT, ["cost" => $cost]);
+    $end = microtime(true);
+} while (($end - $start) < $timeTarget);
+
+echo "Appropriate Cost Found: " . $cost . "\n";
+?>
