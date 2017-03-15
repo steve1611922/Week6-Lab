f0038aecf3eff4dd46fde4c1b9eabe1366068c22
steve1611922 <1611922@rgu.ac.uk>
2017-03-15 12:06:04 +0000

    initial

--- hashStuff.php
+++ hashStuff.php
@@ -3,7 +3,7 @@ $data = "hello";
 
 foreach (hash_algos() as $v) {
     $r = hash($v, $data, false);
-    printf('%-12s %3d %s \n', $v, strlen($r), $r);
+    echo $v. ":".strlen($r).":".$r."<br>";
 }
 ?>
 <?php
